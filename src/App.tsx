49933d95f07ea08e7f8683b5913a8849e4b27c03
shreejan shrestha <shr.shreejan@gmail.com>
2024-04-08 15:51:45 +0545

    UI setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 const App = () => {
-    return <></>;
+    return (
+        <>
+            <div className="container p-4">
+                <div className="h-full border-2 border-black rounded-xl"></div>
+            </div>
+        </>
+    );
 };
 
 export default App;
