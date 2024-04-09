2c19b38bdf098b1d428deec53da846d718ddf27d
shreejan shrestha <shr.shreejan@gmail.com>
2024-04-09 20:23:11 +0545

    Base layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ const App = () => {
     return (
         <>
             <div className="container p-4">
-                <div className="h-full border-2 border-black rounded-xl"></div>
+                <div className="grid-top-container h-1/2 pb-4">
+                    <div className="h-full border-2 border-black rounded-xl"></div>
+                    <div className="h-full border-2 border-black rounded-xl"></div>
+                    <div className="flex flex-col gap-4">
+                        <div className="h-full"></div>
+                        <div className="h-full border-2 border-black rounded-xl"></div>
+                    </div>
+                </div>
+                <div className="grid-bottom-container h-1/2">
+                    <div className="h-full border-2 border-black rounded-xl"></div>
+                    <div className="h-full border-2 border-black rounded-xl"></div>
+                </div>
             </div>
         </>
     );
