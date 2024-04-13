6b438905bdd390dae43848997eb886a8f65ab0b2
shreejan shrestha <shr.shreejan@gmail.com>
2024-04-13 21:27:48 +0545

    Profile card layout

---
+++ src/components/Introduction.tsx
@@ -0,0 +1,30 @@
+const Introduction = () => {
+    return (
+        <>
+            <div className="flex flex-col p-4">
+                <h1 className="text-6xl">SHREEJAN SHRESTHA</h1>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Obcaecati quisquam maiores aliquid aperiam deserunt. Quia
+                    repudiandae rem fuga suscipit pariatur nulla, asperiores
+                    aliquam exercitationem nemo incidunt, odio facere ratione
+                    voluptatibus? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Obcaecati quisquam maiores aliquid aperiam
+                    deserunt. Quia repudiandae rem fuga suscipit pariatur nulla,
+                    asperiores aliquam exercitationem nemo incidunt, odio facere
+                    ratione voluptatibus? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Obcaecati quisquam maiores aliquid aperiam
+                    deserunt. Quia repudiandae rem fuga suscipit pariatur nulla,
+                    asperiores aliquam exercitationem nemo incidunt, odio facere
+                    ratione voluptatibus? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Obcaecati quisquam maiores aliquid aperiam
+                    deserunt. Quia repudiandae rem fuga suscipit pariatur nulla,
+                    asperiores aliquam exercitationem nemo incidunt, odio facere
+                    ratione voluptatibus?
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Introduction;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+const Profile = () => {
+    return (
+        <>
+            <div className="h-full p-2">
+                <img
+                    className="h-full w-full object-cover border-2 border-black rounded-lg"
+                    src="https://github.com/shadcn.png"
+                    alt="Profile Picture"
+                />
+            </div>
+        </>
+    );
+};
+
+export default Profile;
