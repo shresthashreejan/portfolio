7488099160db31966d561649651e49b0d2afb906
shreejan shrestha <shr.shreejan@gmail.com>
2024-04-23 19:58:54 +0545

    Added infinite scroller and setup basic layout for contact page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="bottom-0 p-4 border-t-2">
+            <div className="flex justify-between">
+                <a href="https://github.com/shresthashreejan" target="_blank">
+                    Shreejan Shrestha
+                </a>
+                <p>2024</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
+import InfiniteScroller from "../components/InfiniteScroller/InfiniteScroller";
+
 const Contact = () => {
+    const [emailClicked, setEmailClicked] = useState(false);
+
+    const handleEmailClick = () => {
+        const email = "shr.shreejan@gmail.com";
+
+        navigator.clipboard
+            .writeText(email)
+            .then(() => {
+                setEmailClicked(true);
+            })
+            .catch((error) => {
+                console.error("Error copying to clipboard: ", error);
+            });
+    };
+
     return (
         <>
-            <div className="flex justify-center py-12 px-24">
-                <h1 className="text-6xl uppercase font-medium">Let's Talk</h1>
+            <div className="flex justify-center py-12">
+                <InfiniteScroller />
+            </div>
+
+            <div className="px-24 flex flex-col h-[50vh] justify-center">
+                <h1 className="text-4xl">
+                    Do you have any awesome project ideas?
+                </h1>
+                <h2 className="text-7xl mt-12">Let's talk.</h2>
+                <h1
+                    className={`text-9xl flex flex-wrap cursor-pointer hover:underline ${
+                        emailClicked ? "text-gray-500" : ""
+                    }`}
+                    onClick={handleEmailClick}
+                >
+                    <span>shr.</span>
+                    <span>shreejan</span>
+                    <span>@gmail.</span>
+                    <span>com</span>
+                </h1>
+                <div className="text-4xl mt-12">
+                    {emailClicked
+                        ? "<Looking forward to talk to you/>"
+                        : "<Click email to copy/>"}
+                </div>
             </div>
         </>
     );
